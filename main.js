f92ec906acc49d52bc040868214138e82ea0d065
unmadebed <kevin.archer@live.com>
2024-01-27 14:48:12 +0000

    Add basic math operators

---
+++ main.js
@@ -0,0 +1,21 @@
+// Basic calculator math operations
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+// Basic operation variables
+
+let num1;
+
+let num2;
+
+let operator;
+
+// Operate function
+
+const operate = (operator, num1, num2) => operator(num1, num2);
