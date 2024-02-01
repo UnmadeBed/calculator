8e089b622699a16035017bd95d4afc8599079d1c
unmadebed <kevin.archer@live.com>
2024-02-01 13:12:47 +0000

    Add value of user input to display

--- main.js
+++ main.js
@@ -19,3 +19,12 @@ let operator;
 // Operate function
 
 const operate = (operator, num1, num2) => operator(num1, num2);
+
+// Display button input
+
+const display = document.querySelector('.display');
+const buttonWrapper = document.querySelector('.wrapper');
+
+let displayValue = buttonWrapper.addEventListener('click', (e) => {
+	display.textContent += e.target.textContent;
+});
